ff34061caf2bb0507bbec4be9d639cbb9e024f1d
TariqKanaan <31568445+TariqKanaan@users.noreply.github.com>
2023-03-28 18:08:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,82 +1,77 @@
-from gtts import gTTS
-from playsound import playsound
-import speech_recognition as sr
-import os
-from tkinter import *
-from tkinter import simpledialog, messagebox
-import pyttsx3
-import datetime
-import socket
+import tkinter as tk
+import tkinter.filedialog as filedialog
+from audio_recorder import AudioRecorder
+from audio_transcriber import AudioTranscriber
 
 
-class TranscribeApp:
-    def __init__(self, master):
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
         self.master = master
-        master.title("Arabic Speech Transcription")
-        master.geometry("600x400")
-        master.config(bg="white")
+        self.grid()
+        self.create_widgets()
+        self.audio_recorder = None
 
-        self.label = Label(master, text="Press the button to start speaking and transcribing.", font=("Helvetica", 16), bg="white")
-        self.label.pack(pady=20)
+    def create_widgets(self):
+        self.rec_button = tk.Button(self, text="Record", command=self.start_recording)
+        self.rec_button.grid(row=0, column=0)
 
-        self.speak_button = Button(master, text="Start", command=self.listen, font=("Helvetica", 14), bg="#4CAF50", fg="white")
-        self.speak_button.pack(pady=10)
+        self.stop_button = tk.Button(self, text="Stop", command=self.stop_recording, state=tk.DISABLED)
+        self.stop_button.grid(row=0, column=1)
 
-        self.save_button = Button(master, text="Save", command=self.save, font=("Helvetica", 14), bg="#2196F3", fg="white")
-        self.save_button.pack(pady=10)
+        self.time_label = tk.Label(self, text="Recording time (seconds):")
+        self.time_label.grid(row=1, column=0)
 
-        self.pause_button = Button(master, text="adjust Microphone", command=self.pause_microphone, font=("Helvetica", 14), bg="#F44336", fg="white")
-        self.pause_button.pack(pady=10)
+        self.time_entry = tk.Entry(self)
+        self.time_entry.grid(row=1, column=1)
 
-        self.speak_text_button = Button(master, text="Speak Transcription", command=self.speak_transcription, font=("Helvetica", 14), bg="#9C27B0", fg="white")
-        self.speak_text_button.pack(pady=10)
+        self.save_button = tk.Button(self, text="Save", command=self.save_file, state=tk.DISABLED)
+        self.save_button.grid(row=2, column=0)
 
-        self.output_text = Text(master, font=("Helvetica", 14), height=10)
-        self.output_text.pack(pady=20, padx=50)
+        self.transcribe_button = tk.Button(self, text="Transcribe", command=self.transcribe_audio, state=tk.DISABLED)
+        self.transcribe_button.grid(row=2, column=1)
 
-        self.r = sr.Recognizer()
-        self.microphone = sr.Microphone()
+        self.quit_button = tk.Button(self, text="Quit", command=self.quit)
+        self.quit_button.grid(row=3, column=1)
 
-        self.engine = pyttsx3.init()
-
-    def listen(self):
-        with self.microphone as source:
-            self.r.adjust_for_ambient_noise(source)
-            audio = self.r.listen(source, phrase_time_limit=10)
-
+    def start_recording(self):
         try:
-            transcription = self.r.recognize_google(audio, language="ar-SA")
-            self.output_text.insert(END, transcription + '\n')
+            record_time = int(self.time_entry.get())
+            if record_time <= 0:
+                raise ValueError("Invalid recording time.")
+        except ValueError as e:
+            tk.messagebox.showerror("Error", str(e))
+            return
 
-            obj = gTTS(text=transcription, lang='ar', slow=False)
-            obj.save('text.mp3')
-            playsound('text.mp3')
+        self.audio_recorder = AudioRecorder(record_time)
+        self.rec_button.config(state=tk.DISABLED)
+        self.stop_button.config(state=tk.NORMAL)
+        self.save_button.config(state=tk.DISABLED)
+        self.transcribe_button.config(state=tk.DISABLED)
+        self.audio_recorder.record()
 
-        except sr.UnknownValueError:
-            messagebox.showinfo("Speech Recognition", "Unable to recognize speech")
-        except sr.RequestError as e:
-            messagebox.showerror("Speech Recognition", f"Could not request results from Google Speech Recognition service; {e}")
+    def stop_recording(self):
+        self.audio_recorder.stop()
+        self.rec_button.config(state=tk.NORMAL)
+        self.stop_button.config(state=tk.DISABLED)
+        self.save_button.config(state=tk.NORMAL)
+        self.transcribe_button.config(state=tk.NORMAL)
 
-    def pause_microphone(self):
-        self.r.energy_threshold = 4000  # a higher value means that the recognizer needs more noise to recognize words
+    def save_file(self):
+        file_path = filedialog.asksaveasfilename(defaultextension=".wav")
+        if file_path:
+            self.audio_recorder.save(file_path)
 
-    def speak_transcription(self):
-        text = self.output_text.get("1.0", END)
-        self.engine.say(text)
-        self.engine.runAndWait()
-
-    def save(self):
-        filename = simpledialog.askstring("Save File", "Enter a filename:")
-        if filename:
-            filepath = os.path.join(os.getcwd(), f"{filename}.txt")
-            with open(filepath, 'a', encoding='utf-8') as f:
-                f.write(self.output_text.get("1.0", END))
-                messagebox.showinfo("Save Successful", f"Transcription saved to {filepath}.")
-        else:
-            messagebox.showerror("Save Error", "Please enter a valid filename.")
+    def transcribe_audio(self):
+        file_path = filedialog.askopenfilename(defaultextension=".wav")
+        if file_path:
+            transcriber = AudioTranscriber(file_path)
+            text = transcriber.transcribe()
+            with open("transcription.txt", "w") as f:
+                f.write(text)
+            tk.messagebox.showinfo("Transcription", f"Transcribed text:\n{text}")
 
 
-if __name__ == '__main__':
-    root = Tk()
-    app = TranscribeApp(root)
-    root.mainloop()
+root = tk.Tk()
+app = Application(master=root)
+app.mainloop()
